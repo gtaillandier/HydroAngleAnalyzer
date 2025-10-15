12543e8dd3b31b38b7e8c064853d9b595a3129dc
Gabrieltaillandier <gabrieltaillandier@gmail.com>
2025-10-15 12:36:39 +0200

    update last ovito

--- hydroangleanalyzer/io_utils.py
+++ hydroangleanalyzer/io_utils.py
@@ -1,8 +1,15 @@
-from ovito.io import import_file
+
 import numpy as np
 import os
 
 def load_dump_ovito(in_path):
+    try:
+        from ovito.io import import_file
+    except ImportError:
+        raise ImportError(
+            "The 'ovito' package is required for load dump_ovito. "
+            "Install it with: pip install HydroAngleAnalyzer[ovito]"
+        )
     pipeline = import_file(in_path)
     # Add necessary modifiers
     return pipeline
